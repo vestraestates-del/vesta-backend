e4fd84b242ef218ac4b56ab0b2338b35f06765ad
vestraestates-del <vestraestates@gmail.com>
2025-10-21 15:42:40 +0300

    Update main.py

--- vesta-backend-render/main.py
+++ vesta-backend-render/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from fastapi import FastAPI
 from database import engine, Base
 from routers import auth, portfolio, vault, invites
